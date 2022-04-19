c1ef21976d0bf11884244f1b68906cd84841b344
shannonkjones <shankj687@gmail.com>
2022-04-19 15:19:27 -0400

    Co-authored-by: Chris Jones <cjones1688@gmail.com> Co-authored-by: Chris Johnson <chrej791@gmail.com>

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,33 @@
-import { StyleSheet } from 'react-native';
+import React from "react";
+import { StyleSheet, TextInput, Button, Alert } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const [text, onChangeText] = React.useState("Default Text");
+  const [number, onChangeNumber] = React.useState(null);
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One Thousand</Text>
+      <Text style={styles.title}>Input Fields</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="Placeholder text"
+        //keyboardType="numeric"
+      />
+      <Button
+        title="Default button"
+        onPress={() => Alert.alert(' Button pressed')}
+      />
     </View>
   );
 }
@@ -29,4 +47,13 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    width: "80%",
+  },
 });
+
