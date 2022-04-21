13be2b275d3bc5a083eacc1a78ca28f7bdbc4b6c
shannonkjones <shankj687@gmail.com>
2022-04-21 12:12:28 -0400

    Added multiline text input field

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,23 +1,30 @@
 import React from "react";
-import { StyleSheet, TextInput, Button, Alert } from 'react-native';
+import {Text, View, StyleSheet, TextInput, Button, Alert, ScrollView, SafeAreaView } from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 
 import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+//import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-  const [text, onChangeText] = React.useState("Default Text");
+  const [text, onChangeText] = React.useState(null);
   const [number, onChangeNumber] = React.useState(null);
   const [selectedOption, setSelectedOption] = React.useState("3");
+  const [selectedSetting, setSelectedSetting] = React.useState("3");
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
       <Text style={styles.title}>Input Fields</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <TextInput
-        style={styles.input}
+        blurOnSubmit
+        multiline
+        numberOfLines={2}
+        returnKeyType="done"
+        style={styles.multilineInput}
         onChangeText={onChangeText}
         value={text}
+        placeholder="Enter notes here..."
       />
       <TextInput
         style={styles.input}
@@ -39,11 +46,24 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
         <Picker.Item label="Option 3" value="3" />
         <Picker.Item label="Option 4" value="4" />
       </Picker>
+      <Picker
+        style={styles.picker}
+        selectedValue={selectedSetting}
+        onValueChange={(itemValue, itemIndex) =>
+          setSelectedSetting(itemValue)
+        }>
+        <Picker.Item label="Forest" value="1" />
+        <Picker.Item label="Garden" value="2" />
+        <Picker.Item label="Commercial Field" value="3" />
+        <Picker.Item label="Urban" value="4" />
+        <Picker.Item label="Suburban" value="4" />
+      </Picker>
       <Button
         title="Default button"
         onPress={() => Alert.alert(' Button pressed')}
       />
-    </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -51,7 +71,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
+  },
+  scrollView: {   
+    width: '90%',
   },
   title: {
     fontSize: 20,
@@ -63,15 +86,20 @@ const styles = StyleSheet.create({
     width: '80%',
   },
   input: {
-    height: 40,
-    margin: 12,
+    margin: 10,
     borderWidth: 1,
     borderRadius: 10,
     padding: 10,
-    width: "80%",
+  },
+  multilineInput: {
+    margin: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    height: 60,
   },
   picker: {
-    width: "80%",
+    width: "100%",
   }
 });
 
